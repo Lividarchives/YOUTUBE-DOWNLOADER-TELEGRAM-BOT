c1975924e80e12ee7a240c4cae792c76360dc1d2
Abdimk <63399762+abdimk@users.noreply.github.com>
2022-10-12 07:37:11 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import  os
 
-BOT_TOKEN = "1896342074:AAE9GSpf2VC3MFGUxUzyZrXgt6CZj7cKPBU"
-APP_ID = 1390245
-API_HASH = "a021871c10d820e0bb7a5d0e999d8c7f"
+BOT_TOKEN =
+APP_ID = 
+API_HASH = 
 
 youtube_next_fetch = 0  
 
